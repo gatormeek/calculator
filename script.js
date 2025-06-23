b819b5c6bc396c4d52b89a5325604e303c76726c
gatormeek <204446387+gatormeek@users.noreply.github.com>
2025-06-22 23:50:57 -0700

    Add clearAll, clearCurrent, backspace, operator
    Add clearing and backspace functions, and add
    operator to the userInput string.

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 let hoverSelector = document.querySelectorAll("button");
-let numbers = document.querySelectorAll(".white-bg");
+let numbers = document.querySelectorAll(".nums");
+let operators = document.querySelectorAll(".operators");
 let userInput = document.querySelector("#input-window");
+let outputWindow = document.querySelector("#output-window");
+let clearCurrent = document.querySelector("#clear-current");
+let clearAll = document.querySelector("#clear-all");
+let backspace = document.querySelector("#backspace");
 userArray = [];
+outputArray = [];
+
+backspace.addEventListener('click', () => {
+    userArray.pop();
+    userInput.textContent = userArray.join('');
+})
+
+clearAll.addEventListener('click', () => {
+    userArray = [];
+    outputArray = [];
+    userInput.textContent = userArray.join('');
+    outputWindow.textContent = '';
+})
+
+clearCurrent.addEventListener('click', () => {
+    userArray = [];
+    userInput.textContent = userArray.join('');
+})
+
+operators.forEach(link => {
+    link.addEventListener('click', () => {
+        userArray.push(` ${link.textContent} `);
+        userInput.textContent = userArray.join('');
+
+    })
+})
+
 numbers.forEach(link => {
     link.addEventListener('click', () => {
         if(link.textContent === "." & userArray.includes(".")) {
